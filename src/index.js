8af435c6a243ff10e62a8bbae9992c88b227428a
kimhyeonju <kimhyesom@naver.com>
2022-07-01 12:49:54 +0900

    🔥

--- src/index.js
+++ src/index.js
@@ -13,11 +13,9 @@ import "slick-carousel/slick/slick-theme.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
