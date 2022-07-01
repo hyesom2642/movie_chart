7b788f94c4715f8128df89e6a996ca86d0c2c999
kimhyeonju <kimhyesom@naver.com>
2022-07-01 12:49:30 +0900

    💄 Style Navbar component

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,28 @@
+// import { AiOutlineMenu, AiOutlineSearch} from "react-icons/ai";
+// import { BsMoon, BsFillBrightnessHighFill} from "react-icons/bs"; 
+
+// > css 
+import style from './Nav.module.css';
+
 const Nav = () => {
   return(
     <>
-      
+      <header className={`container ${style.container__header}`}>
+        <div className={`${style.left}`}>
+          <a href="/" className={`${style.logo}`} aria-label="logo">
+            MOVIE
+          </a>
+          {/* <AiOutlineMenu className={`${style.ic__menu}`}/> */}
+        </div>
+        {/* <div className="center">
+          <input placeholder='Find Your Love Movies' />
+          <AiOutlineSearch />
+        </div> */}
+        <div className={`${style.right}`}>
+          {/* <BsMoon className={`${style.ic__moon}`} />
+          <BsFillBrightnessHighFill className={`${style.ic__sun}`}/> */}
+        </div>
+      </header>
     </>
   )
 }
