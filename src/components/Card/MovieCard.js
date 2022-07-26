702b238b1dc5da03ac742aecfa4e105c772eb803
kimhyeonju <kimhyesom@naver.com>
2022-07-26 17:01:49 +0900

    ✨ Implement MovieCard for UpComingSlider & TopRatedSlider

---
+++ src/components/Card/MovieCard.js
@@ -0,0 +1,68 @@
+// > styled components
+import styled from 'styled-components';
+
+// > react-router-dom@6 
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+const MovieCard = ({id, title, posterUrl, vote}) => {
+    const [show, setShow] = useState(false);
+
+
+    return(
+        <>
+            <Link to={`/movie/${id}`}>
+                <Movie onMouseMove={() => {setShow(true)}}
+                        onMouseLeave={() => {setShow(false)}}>
+                    <img src={posterUrl} alt={title} 
+                    />
+                    {
+                        show === true 
+                        ? <TextBox>
+                            <p>{title}</p>
+                            <p>{vote} / 10</p>
+                        </TextBox>
+                        : null
+                    }
+
+                </Movie>
+            </Link>
+        </>
+    )
+}
+export default MovieCard;
+
+const Movie = styled.div`
+    position: relative;
+    width: 200px;
+    height: 300px;
+    transition: all 0.3s ease-in-out;
+
+    &:hover {
+        opacity: 0.8;
+        transform: scale(1.05);
+    }
+
+    img {
+        width: 100%;
+        height: 100%;
+    }
+`;
+
+const TextBox= styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    color: #fff;
+    display: flex;
+    width: 100%;
+    height: 100%;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    p {
+        font-size: 18px;
+        font-weight: bold;
+    }
+`;
