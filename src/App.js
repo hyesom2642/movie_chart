8c02a1bf15705f1b07f242ba49188beb58a3c3c8
kimhyeonju <kimhyesom@naver.com>
2022-07-31 19:18:23 +0900

    ✨ Add Routes of MovieMenu & Detail Page, Set up Loading

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 // > react-router-dom@6 
 import { Routes, Route } from 'react-router-dom';
 
+// > 
+import { useState, useEffect } from 'react';
+
 // > components 
 import Nav from './components/Nav/Nav';
 import Home from './components/Routes/Home';
+import MovieMenu from './components/Routes/MovieMenu';
+import Detail from './components/Routes/Detail';
+import Loading from './components/Loading/Loading';
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect( () => {
+    setLoading(false);
+  }, [])
+
   return(
     <>
-      <Nav />
-      <Routes>
-        <Route path={"/"} element={<Home />} />
-      </Routes>
+    {
+      loading === true
+      ? <Loading />
+      :
+      <>
+        <Nav />
+        <Routes>
+          <Route path={"/"} element={<Home />} />
+          <Route path={`/page/:menu`} element={<MovieMenu />} />
+          <Route path={`/movie/:movieId`} element={<Detail />} />
+        </Routes>
+      </>
+    }
     </>
   )
 }
