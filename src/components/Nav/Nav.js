a09b13dcb5b37791c1cf8374ca8f7b4aabe89700
kimhyeonju <kimhyesom@naver.com>
2022-08-05 00:03:08 +0900

    🐛 Fix error

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,20 +1,18 @@
 // > styled components 
 import styled from 'styled-components';
-import { GiHamburgerMenu } from "react-icons/gi";
 
 // > components 
 import NavItem from './NavItem';
 
 // > data 
 import NavItem_Obj from './NavData';
-import NavSign from './NavSign';
 
 // > 
 import { useState, useEffect } from 'react';
 
 const Nav = () => {
   const [scrollY, setScrollY] = useState(0);
-  
+
   const scrollPosition = () => {
     setScrollY(window.scrollY);
   }
@@ -46,24 +44,4 @@ const Container = styled.div`
   &.bgColor {
     background-color: #000;
   }
-`;
-
-const MenuIcon= styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 30px;
-    height: 30px;
-    font-size: 30px;
-    color: #fff;
-    cursor: pointer;
-
-    @media ${props => props.theme.tablet} {
-        width: 40px;
-        height: 40px;
-        font-size: 40px;
-    }
-    @media ${props => props.theme.desktop} {
-        display: none;
-    }
 `;
