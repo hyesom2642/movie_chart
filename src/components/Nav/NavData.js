b027f0448e7c430277643bbfbaed85f93e6744b3
kimhyeonju <kimhyesom@naver.com>
2022-08-11 17:44:59 +0900

    🍱 Add NavSignMenu of NavData

--- src/components/Nav/NavData.js
+++ src/components/Nav/NavData.js
@@ -1,19 +1,30 @@
-const NavData = [
-  {
-      title: "상영중인 영화",
-      path: "now_playing",
-  },
-  {
-      title: "인기 영화",
-      path: "popular",
-  },
-  {
-      title: "영화 평점순",
-      path: "top_rated",
-  },
-  {
-      title: "상영예정작",
-      path: "up_coming",
-  },
+const NavMenu = [
+    {
+        title: "상영중인 영화",
+        path: "now_playing",
+    },
+    {
+        title: "인기 영화",
+        path: "popular",
+    },
+    {
+        title: "영화 평점순",
+        path: "top_rated",
+    },
+    {
+        title: "상영예정작",
+        path: "up_coming",
+    }
 ]
-export default NavData;
+
+const NavSignMenu = [
+    {
+        title: "회원가입",
+        path: "",
+    },
+    {
+        title: "로그인",
+        path: "",
+    },
+]
+export { NavMenu, NavSignMenu };
