a10493cbcdc7fbf6f040d64c313ea5d3db108027
kimhyeonju <kimhyesom@naver.com>
2022-08-20 23:40:46 +0900

    Add Api Key

---
+++ src/Api.js
@@ -0,0 +1,11 @@
+const API_KEY = "66cd88435216ca2fea9f8bc982f99904";
+
+const BASE_PATH = "https://api.themoviedb.org/3";
+
+const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
+
+const BASE_LANG = 'ko';
+
+const BASE_REGION = 'KR';
+
+export { API_KEY, BASE_PATH, IMAGE_BASE_URL, BASE_LANG, BASE_REGION};
